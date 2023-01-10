b20320bb4e04f534963b2078b82f0895a755fe30
REPPL <REPPL@proton.me>
2023-01-10 19:20:33 +0000

    Initial commit

---
+++ inits/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(markdown-mode powerthesaurus cyberpunk-theme synosaurus exec-path-from-shell page-break-lines elpy ace-jump-mode highlight-symbol try counsel auto-complete helm-swoop helm-ag helm-projectile helm smooth-scrolling nyan-mode use-package)))
+(custom-set-faces
+ '(highlight ((t (:foreground nil :background "#101025"))))
+ '(org-block ((t (:background "#101010"))))
+ '(org-block-begin-line ((t (:foreground "#008ED1" :background "#151515"))))
+ '(org-block-end-line ((t (:foreground "#008ED1" :background "#151515"))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+ '(region ((t (:background "#453D40")))))
